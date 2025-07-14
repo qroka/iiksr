c1b5f1cf01d81db3f2bf829da6138c506e45d09f
qroka <kolomiets@sokral.ru>
2025-07-14 14:40:03 +0500

    :wc

---
+++ app/pages/main-page.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+    return (
+      <main className="min-h-screen bg-white p-6">
+        <h1 className="text-4xl font-bold text-blue-700">Сургутский район</h1>
+        <p className="mt-4 text-lg text-gray-800">
+          Добро пожаловать на информационный портал о Сургутском районе!
+        </p>
+      </main>
+    );
+  }
+  
